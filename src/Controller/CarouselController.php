bd71b588032e767f6907b7c233aaccb3bbffec3d
TEISSIER Thomas <tigerhead28@hotmail.com>
2024-08-19 11:03:58 +0200

    Modification emplacement photos , avancé de la partie albums photos correct, manque améliorations des boutons

--- src/Controller/CarouselController.php
+++ src/Controller/CarouselController.php
@@ -2,21 +2,36 @@
 
 namespace App\Controller;
 
-use App\Repository\DossierRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Finder\Finder;
 
 class CarouselController extends AbstractController
 {
     #[Route('/carousel', name: 'app_carousel')]
-     
-    public function index(DossierRepository $dossierRepository): Response
+    public function index(Request $request): Response
     {
-        $dossiers = $dossierRepository->findAll();
+        $imageDirectories = ['Cequevoitfox', 'Conversation', 'Delirium', 'Festivaloffavignon', 'Huitfemmes', 'Jeuxdecannes', 'Lagrandemuraille', 'Lespalmes', 'Lesrats', 'Mastication', 'Naïs', 'Perenoelordure', 'Soudainletedernier', 'Uncoindazur', 'Uneheure' ]; // Liste des dossiers disponibles
+        $selectedDirectory = $request->query->get('directory', $imageDirectories[0]);
+
+        if (!in_array($selectedDirectory, $imageDirectories)) {
+            throw $this->createNotFoundException('Le dossier sélectionné n\'existe pas.');
+        }
+
+        $images = [];
+        $finder = new Finder();
+        $finder->files()->in($this->getParameter('kernel.project_dir') . '/public/images/' . $selectedDirectory);
+
+        foreach ($finder as $file) {
+            $images[] = 'images/' . $selectedDirectory . '/' . $file->getFilename();
+        }
 
         return $this->render('carousel/index.html.twig', [
-            'dossiers' => $dossiers,
+            'images' => $images,
+            'directories' => $imageDirectories,
+            'selectedDirectory' => $selectedDirectory,
         ]);
     }
 }
